739e323adf50d8e4fa25dcff1afc7f5781f89e5a
eugen <eugen@DESKTOP-SR42LU0>
2021-04-12 13:15:27 +0900

    이현진 수정

--- src/main/java/com/spring/project/test.java
+++ src/main/java/com/spring/project/test.java
@@ -2,7 +2,7 @@ package com.spring.project;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("������ �Դϴ�.");
-		System.out.println("하지민 입니다");
+		System.out.println("이현진 입니다.");
+		System.out.println("하지민 입니다.");
 	}
 }
